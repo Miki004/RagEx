b05c6f679a5f3589896491f37879c0331899605c
Miki004 <162804895+Miki004@users.noreply.github.com>
2025-08-04 16:42:19 +0200

    Refactor to implement number guessing game logic
    Replaces previous arithmetic operation logic with a number guessing game. Introduces new state fields, game setup, guessing, hint, and end nodes, and updates the state graph accordingly. Removes unused arithmetic functions and related graph nodes.

--- src/indexing.py
+++ src/indexing.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from typing import Annotated,List
 from typing_extensions import TypedDict
-
+import numpy as np
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
 from langchain.chat_models import init_chat_model
@@ -17,78 +17,77 @@ tavly_key = os.getenv("TAVILY_API_KEY")
 llm = init_chat_model("openai:gpt-4.1")
 
 class State(TypedDict):
-    numeber1 : int
-    numeber2 : int 
-    operation: str
-    numeber3 : int
-    intermediate_number: int
-    final_number : int
-    operation2 : str
+    number : int 
+    counter: int  
+    won: bool
+    player_name : str
+    gambling : int
 
-def move_by_operation(state: State) -> State:
-    if state["operation"] == "+":
-        return "add_numbers"
-    elif state["operation"]== "-":
-        return "subtract_numbers"
-    
-def move_by_operation2(state: State) -> State:
-    if state["operation2"] == "+":
-        return "add_numbers"
-    elif state["operation2"]== "-":
-        return "subtract_numbers"        
 
-def add(state:State) -> State:
-    state["numeber3"] = state["numeber1"] + state["numeber2"]
-    return state
-
-def subtract(state: State) -> State:
-    state["numeber3"] = state["numeber1"] - state["numeber2"]
+def setup(state:State) -> State:
+    state["number"] = np.random.randint(1,20)
+    state["counter"] = 0
+    state["won"] = False
     return state
 
 
-def add2(state:State) -> State:
-    state["final_number"] = state["numeber3"] + state["intermediate_number"]
+def should_continue(state:State) -> State:
+    if state["won"]:
+        return "end_game"
+    if state["counter"] < 7:
+        return "retry_gambling"
+    else:
+        state["won"] = False
+        return "end_game"
+
+def suggest_number(state:State) -> State:
+    if not state["won"]:
+        if state["gambling"] > state["number"]:
+            print("Guess something lower!")
+        else:
+            print("Guess something higher!")
     return state
 
-def subtract2(state: State) -> State:
-    state["final_number"] = state["numeber3"] - state["intermediate_number"]
+def guess(state:State) -> State:
+    print(f"Gambling attempt: {state["counter"]}\n")
+    state["gambling"] = int(input("Guess a number: "))
+    state["counter"] += 1
+    if state["gambling"] == state["number"]:
+        state["won"] = True
     return state
+
+def end_node(state:State) -> State:
+    if state["won"]:
+        print("You won guessing the " + str(state["number"]))
+    else:
+        print("You lost the number was " + str(state["number"]))
+
 
 graph_builder = StateGraph(State)
-graph_builder.add_node("add_node", add)
-graph_builder.add_node("subtract_node", subtract)
+graph_builder.add_node("setup_node", setup)
+graph_builder.add_edge(START, "setup_node")
+graph_builder.add_node("guess_node", guess)
+graph_builder.add_node("end_node", end_node)
+graph_builder.add_node("hint_node", suggest_number)
+graph_builder.add_edge("setup_node", "guess_node")
+graph_builder.add_edge("guess_node", "hint_node")
 
-graph_builder.add_node("add_node2", add2)
-graph_builder.add_node("subtract_node2", subtract2)
-
-graph_builder.add_node("router", lambda state:state) # stiamo dicendo che l'input state sarà l'output state
-graph_builder.add_node("router2", lambda state:state)
-graph_builder.add_edge(START, "router") # il nodo router è qualcosa che non viene definito esplicitamente
-
-graph_builder.add_conditional_edges(
-    "router",
-    move_by_operation,
-    {
-        "add_numbers": "add_node",
-        "subtract_numbers": "subtract_node"
-
-    }
-)
-graph_builder.add_edge("add_node", "router2")
-graph_builder.add_edge("subtract_node", "router2")
 
 graph_builder.add_conditional_edges(
-    "router2",
-    move_by_operation2,
+    "hint_node",
+    should_continue,
     {
-        "add_numbers": "add_node2",
-        "subtract_numbers": "subtract_node2"
-
+        "retry_gambling": "guess_node",
+        "end_game": "end_node"
     }
 )
-graph_builder.add_edge("add_node2", END)
-graph_builder.add_edge("subtract_node2",END)
+graph_builder.add_edge("end_node", END)
 
 app = graph_builder.compile()
-result = app.invoke({"numeber1": 5, "numeber2": 3, "operation": "+", "intermediate_number": 2, "operation2": "-"})
-print(result)
+# from PIL import Image
+# import io
+
+# img_bytes = app.get_graph().draw_mermaid_png()
+# image = Image.open(io.BytesIO(img_bytes))
+# image.show() 
+app.invoke({"player_name": "Michele "})
